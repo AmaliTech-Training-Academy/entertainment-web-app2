083ff824375642f1599990b0ba4583e23f4a5243
ericmanu <eric.manu@amalitech.com>
2023-02-27 16:37:45 +0000

    Worked on the hover stated and completed the tablet responsiveness

---
+++ src/script.js
@@ -0,0 +1,8 @@
+const navLinks = document.querySelectorAll("a");
+const activePage = window.location.pathname;
+
+navLinks.forEach(link => {
+    if(link.href.includes(activePage)){
+        link.classList.add('active');
+    }
+});
--- src/series.js
+++ src/series.js
@@ -34,39 +34,50 @@ fetch(json_url).then(Response => Response.json())
         +'<div class="bookmark-btn">'
         +'<svg width="12" height="14" xmlns="http://www.w3.org/2000/svg"><path d="m10.518.75.399 12.214-5.084-4.24-4.535 4.426L.75 1.036l9.768-.285Z"/></svg>'
         +'</div>'
-        +'<div class="play-items">'
         +'<div class="play">'
         +'<div class="play-btn"></div>'
-        // +'<p>Play</p>'
-        +'</div>'
+        +'<p>Play</p>'
         +'</div>'
         +'</div>' ;
         }
         seriesContainer.innerHTML = series;    
     }
 
-    // const recommended = document.querySelectorAll(".option");
+    const recommended = document.querySelectorAll(".series");
 
-    //     recommended.forEach(option => {
-    //         const img = option.querySelector(".series-image");
-    //         const play = option.querySelector(".play");
-    //         const bookmark = option.querySelector(".bookmark-btn");
-    //         img.addEventListener('mouseover', () => {
-    //             img.classList.add('dark')
-    //             play.classList.add('show')
-    //         });
-    //         img.addEventListener('mouseout', () => {
-    //             img.classList.remove('dark')
-    //             play.classList.remove('show')
-    //         });
-    //         play.addEventListener('mouseover', () => {
-    //             img.classList.add('dark')
-    //             play.classList.add('show')
-    //         });
-    //         bookmark.addEventListener('mouseover', () => {
-    //             img.classList.add('dark')
-    //             play.classList.add('show')
-    //         });
 
-    //     });
+        recommended.forEach(seriesItems => {
+            const img = seriesItems.querySelector(".series-image");
+            const play = seriesItems.querySelector(".play");
+            const bookmark = seriesItems.querySelector(".bookmark-btn");
+            const button = seriesItems.querySelector("svg");
+            img.addEventListener('mouseover', () => {
+                img.classList.add('dark')
+                play.classList.add('show')
+            });
+            img.addEventListener('mouseout', () => {
+                img.classList.remove('dark')
+                play.classList.remove('show')
+            });
+            play.addEventListener('mouseover', () => {
+                img.classList.add('dark')
+                play.classList.add('show')
+            });
+            bookmark.addEventListener('mouseover', () => {
+                // img.classList.add('dark')
+                // play.classList.add('show')
+                button.classList.add('choice')
+            });
+            bookmark.addEventListener('mouseout', () => {
+                button.classList.remove('choice')
+            })
+            bookmark.addEventListener('click', ()=>{
+                if(button.classList.contains('keep')){
+                    button.classList.remove('keep')
+                }
+                else{
+                    button.classList.add('keep')
+                }
+            });
+        });
 })

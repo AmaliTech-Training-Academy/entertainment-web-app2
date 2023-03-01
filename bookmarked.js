5eed2cce643c476d3bccf9b382ec58a05217661f
Benedict Arhin <benedict.arhin@amalitech.com>
2023-03-01 08:39:43 +0000

    Bookmarked js

--- bookmarked.js
+++ bookmarked.js
@@ -66,4 +66,4 @@ function removeMovieFromBookmark(movieId) {
   let bookmarkedMovies = JSON.parse(localStorage.getItem('bookmarkedMovies')) || [];
   bookmarkedMovies = bookmarkedMovies.filter(movie => movie.id !== movieId);
   localStorage.setItem('bookmarkedMovies', JSON.stringify(bookmarkedMovies));
-}
+}*/

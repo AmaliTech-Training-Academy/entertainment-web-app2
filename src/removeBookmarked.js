a5ae587192cd432f8f41698eb8d4af40e4b1b41b
ericmanu <eric.manu@amalitech.com>
2023-03-10 16:07:45 +0000

    Added new css file and new lines

--- src/removeBookmarked.js
+++ src/removeBookmarked.js
@@ -1,17 +1,3 @@
-// const bookmarkedVideo = document.getElementById('.bookmark-btn');
-// bookmarkedVideo.remove();
-
-// function removeBookmark(bookmarkId) {
-//     let bookmarks = JSON.parse(localStorage.getItem(".bookmark.btn")) || [];
-//     bookmarks = bookmarks.filter(function(bookmark) {
-//       return bookmark.id !== bookmarkId;
-//     });
-//     localStorage.setItem(".bookmark.btn", JSON.stringify(bookmarks));
-//     const bookmarkLink = document.querySelector(`[href="#bookmark-${bookmarkId}"]`);
-//     if (bookmarkLink) {
-//       bookmarkLink.parentNode.remove();
-//     }
-//   }
 function removeBookmark() {
     const bookMarkedButton = document.querySelectorAll(".bookmark-btn");
     bookMarkedButton.forEach((btn) => (btn.onclick = () => {
@@ -29,48 +15,3 @@ function removeBookmark() {
   }
   
   export { removeBookmark };
-  
-
-// function removeBookmark() {
-//     const bookMarkedButton = document.querySelectorAll(".bookmark-btn");
-//     bookMarkedButton.forEach(
-//         (btn) => (btn.onclick = () =>
-//         {
-//             const title = btn.previousElementSibling.children[1].textContent;
-//             removeMovie(title);
-//         })
-//     );
-
-//     function removeMovie(movie) {
-//         const bookmarkedMovies = 
-//             JSON.parse(localStorage.getItem(".bookmark.btn")) || [];
-//         console.log(bookmarkedMovies);
-//         bookmarkedMovies.remove(movie);
-//         localStorage.setItem("bookmarkedMovies", JSON.stringify(bookmarkedMovies));
-//     }
-// }
-
-// export { removeBookmark };
-
-// function addBookmark() {
-//     //Listening for form submit
-//     const bookMarkedButton = document.querySelectorAll(".bookmark-btn");
-  
-//     bookMarkedButton.forEach(
-//       (btn) =>
-//         (btn.onclick = () => {
-//           const title = btn.previousElementSibling.children[1].textContent;
-//           addMovieToBookmark(title);
-//         })
-//     );
-  
-//     function addMovieToBookmark(movie) {
-//       const bookmarkedMovies =
-//         JSON.parse(localStorage.getItem("bookmarkedMovies")) || [];
-//       console.log(bookmarkedMovies);
-//       bookmarkedMovies.push(movie);
-//       localStorage.setItem("bookmarkedMovies", JSON.stringify(bookmarkedMovies));
-//     }
-//   }
-  
-//   export { addBookmark };

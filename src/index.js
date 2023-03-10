78cc2c0301ad37c2768eac0b8f8154bd2ffb1a82
Benedict Arhin <benedict.arhin@amalitech.com>
2023-03-10 16:33:53 +0000

    Search bar.

--- src/index.js
+++ src/index.js
@@ -48,7 +48,7 @@ fetch(json_url).then(Response => Response.json())
             trendsContainer.innerHTML = trend;
         }
 
-        for(i = 0; i < data.length; i++){
+        for(let i = 0; i < data.length; i++){
             if(!data[i].isTrending){
                 options += 
             '<div class="option">' 
@@ -242,4 +242,42 @@ fetch(json_url).then(Response => Response.json())
                     info.forEach(about => about.classList.remove("light-text"));
         }
     addBookmark();
+
+    const resultsContainer = document.querySelector(".searchItems");
+console.log(resultsContainer);
+
+const input = document.querySelector('input');
+input.addEventListener('keydown', function(event) {
+    if (event.key === 'Enter') {
+        window.location.href = "searchResults.html"
+    console.log('Enter key was pressed');
+    }
+});
+
+    let results = "";
+    input.addEventListener('keyup', () => {
+        const queryValue = input.value;
+        console.log(queryValue);
+        Results(queryValue);
+        })
+
+    function Results(query){
+        data.forEach(element => {
+            if(element.title.toLowerCase().includes(query.toLowerCase())){
+                // window.location.href = "searchResults.html"
+                results +=  '<li>'
+                            +'<img class="search-item-img" src="'+element.thumbnail.regular.large+'"/>'
+                            +'<div class="search-item-info">'
+                            +'<div class="search-item-title">'
+                            +element.title
+                            +'</div>'
+                            +'<div class="search-item-year">'
+                            +element.year
+                            +'</div>'
+                            +'</div>'
+                            +'</li>';
+                        }
+                        resultsContainer.innerHTML = results;
+                })
+            }
     });

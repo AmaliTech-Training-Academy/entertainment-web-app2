da29206c697bfe4dbda8b350f6c9405c81ce3028
ericmanu <eric.manu@amalitech.com>
2023-03-14 16:30:47 +0000

    Working on remove bookmark functionality on other pages

--- src/removeBookmarked.js
+++ src/removeBookmarked.js
@@ -17,3 +17,24 @@ function removeBookmark() {
   }
 
   export { removeBookmark };
+
+
+//   function removeBookmark() {
+//     const bookMarkedButton = document.querySelectorAll(".bookmark-btn");
+//     bookMarkedButton.forEach((btn) => (btn.onclick = () => {
+//             const title = btn.previousElementSibling.children[1].textContent;
+//             removeMovie(title);
+//             btn.parentElement.parentElement.remove(); 
+//         })
+//     );
+
+//     function removeMovie(movie) {
+//         const bookmarkedMovies = JSON.parse(localStorage.getItem("bookmarkedMovies")) || [];
+//         console.log(bookmarkedMovies);
+//         bookmarkedMovies.splice(bookmarkedMovies.indexOf(movie), 1);
+//         console.log(bookmarkedMovies);
+//         localStorage.setItem("bookmarkedMovie", JSON.stringify(bookmarkedMovies));
+//     }
+// }
+
+// export { removeBookmark };

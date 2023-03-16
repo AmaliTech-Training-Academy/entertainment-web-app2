b8319408c525cb6df68f9e50de8673b618fa7e5a
ericmanu <eric.manu@amalitech.com>
2023-03-16 16:27:44 +0000

    Worked on how to preserve filled button state

--- src/bookmarked.js
+++ src/bookmarked.js
@@ -2,17 +2,14 @@ function addBookmark() {
   //Listening for form submit
   const bookMarkedButton = document.querySelectorAll(".bookmark-btn");
 
-  bookMarkedButton.forEach(
-    (btn) =>
-      (btn.onclick = () => {
+  bookMarkedButton.forEach((btn) => (btn.onclick = () => {
         const title = btn.previousElementSibling.children[1].textContent;
         addMovieToBookmark(title);
       })
   );
 
   function addMovieToBookmark(movie) {
-    const bookmarkedMovies =
-      JSON.parse(localStorage.getItem("bookmarkedMovies")) || [];
+    const bookmarkedMovies = JSON.parse(localStorage.getItem("bookmarkedMovies")) || [];
     console.log(bookmarkedMovies);
     bookmarkedMovies.push(movie);
     localStorage.setItem("bookmarkedMovies", JSON.stringify(bookmarkedMovies));

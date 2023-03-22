6230d18285c5889a834cbac49b43fc380f8904f9
Godbless-Abbew <godbless.abbew@amalitech.com>
2023-03-22 11:18:06 +0000

    made some changes

--- src/search2.js
+++ src/search2.js
@@ -10,7 +10,7 @@ fetch(json_link)
         data.forEach(element => {
             if(element.title.toLowerCase().includes(query.toLowerCase())){
                 results +=  '<div class="search-item">'
-                            +'<img class="search-item-img" src="'+element.thumbnail+'"/>'
+                            +'<img class="search-item-img" src="'+element.thumbnail.regular.large+'"/>'
                             +'<div class="search-item-info">'
                             +'<div class="search-item-title">'
                             +element.title
